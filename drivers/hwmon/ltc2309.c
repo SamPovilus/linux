b98149b9daf3e1f14a548d677392adc9f7466f44
Sam Povilus <kernel.development@povil.us>
2017-03-23 18:38:04 -0600

    comiples

--- drivers/hwmon/ltc2309.c
+++ drivers/hwmon/ltc2309.c
@@ -31,6 +31,7 @@
 #include <linux/module.h>
 #include <linux/regmap.h>
 #include <linux/slab.h>
+#include <linux/printk.h>
 
 /* The LTC2309 registers */
 #define LTC2309_CMD_REG 0
@@ -64,23 +65,22 @@ static ssize_t ltc2309_show_in(struct device *dev, struct device_attribute *da,
 	struct sensor_device_attribute *attr = to_sensor_dev_attr(da);
 	struct ltc2309_data *data = dev_get_drvdata(dev);
 	u8 cmd = ltc2309_cmd_byte(data->cmd_byte, attr->index);
-	unsigned int regval;
 	s32 ret;
 
-	ret = i2c_smbus_write_byte(dev_get_drvdata(dev)->client, cmd);
+	ret = i2c_smbus_write_byte(data->client, cmd);
 	if(ret < 0)
 	{
-		pr_error("could not write to device\n");
+		pr_err("could not write to device\n");
 	}
 	/* As far as I can tell the command word will be ignored */
-	ret = i2c_smbus_read_word_data(dev_get_drvdata(dev)->client, 0);
+	ret = i2c_smbus_read_word_data(data->client, 0);
 	if (ret < 0)
 	{
-		pr_error("could not read from device");
+		pr_err("could not read from device");
 	}
 	ret = ret >> 4;
 	return sprintf(buf, "%d\n",
-		       DIV_ROUND_CLOSEST(regval * 4096, 1000));
+		       DIV_ROUND_CLOSEST(ret * 4096, 1000));
 }
 
 static SENSOR_DEVICE_ATTR(in0_input, S_IRUGO, ltc2309_show_in, NULL, 0);
@@ -113,9 +113,6 @@ static int ltc2309_probe(struct i2c_client *client,
 	struct ltc2309_data *data;
 	struct device *hwmon_dev;
 	unsigned int vref_mv = LTC2309_INT_VREF_MV;
-	bool diff_input = false;
-	bool ext_vref = false;
-	unsigned int regval;
 
 	data = devm_kzalloc(dev, sizeof(struct ltc2309_data), GFP_KERNEL);
 	if (!data)
